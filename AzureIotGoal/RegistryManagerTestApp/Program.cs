5dd01ad6ddd0a759a2f5f6619dbaa5f2ef4c8047
Paul Grebenc <pgrebenc@carbonite.com>
2019-01-10 12:30:19 -0500

    Get hub connection string from app settings.

--- AzureIotGoal/RegistryManagerTestApp/Program.cs
+++ AzureIotGoal/RegistryManagerTestApp/Program.cs
@@ -1,16 +1,37 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Azure.Devices;
+using Microsoft.Extensions.Configuration;
 
 namespace RegistryManagerTestApp
 {
     class Program
     {
-        private const string IotHubConnectionString = ""; // TODO: Get from settings.
+        private readonly string _iotHubConnectionString;
 
         static async Task Main(string[] args)
         {
             await new Program().Run();
+        }
+
+        private Program()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            try
+            {
+                _iotHubConnectionString = configuration["IotHubConnectionString"];
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"Failed to load app settings: {e.Message}");
+                Console.Error.WriteLine("Please ensure that a valid appsettings.json file is present.");
+                Environment.Exit(-1);
+            }
         }
 
         private async Task Run()
@@ -43,7 +64,7 @@ namespace RegistryManagerTestApp
         {
             Console.WriteLine("Displaying all currently registered devices:");
 
-            var registryManager = RegistryManager.CreateFromConnectionString(IotHubConnectionString);
+            var registryManager = RegistryManager.CreateFromConnectionString(_iotHubConnectionString);
 
             var devicesQuery = registryManager.CreateQuery("select * from devices");
 
