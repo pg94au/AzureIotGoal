cb8e92b91893dea57054e69bfeaaa7b0c830216b
Paul Grebenc <pgrebenc@carbonite.com>
2019-01-10 12:08:55 -0500

    Initial app that can list current devices in registry.

--- AzureIotGoal/RegistryManagerTestApp/Program.cs
+++ AzureIotGoal/RegistryManagerTestApp/Program.cs
@@ -1,12 +1,67 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
 
 namespace RegistryManagerTestApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private const string IotHubConnectionString = "HostName=iot-hub-paul.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=kSSGc6PoxbHdZRKzIEiPYL4IHo2tDvUmo21FSQ45OUg=";
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await new Program().Run();
+        }
+
+        private async Task Run()
+        {
+            while (true)
+            {
+                Console.WriteLine("1 - Display all currently registered devices");
+                Console.WriteLine("X - Exit");
+                Console.WriteLine();
+
+                var selection = Console.ReadKey(true);
+
+                switch (char.ToUpperInvariant(selection.KeyChar))
+                {
+                    case '1':
+                        await DisplayAllExistingDevices();
+                        break;
+                    case 'X':
+                        return;
+                    default:
+                        Console.WriteLine("Choose again!");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private async Task DisplayAllExistingDevices()
+        {
+            Console.WriteLine("Displaying all currently registered devices:");
+
+            var registryManager = RegistryManager.CreateFromConnectionString(IotHubConnectionString);
+
+            var devicesQuery = registryManager.CreateQuery("select * from devices");
+
+            while (devicesQuery.HasMoreResults)
+            {
+                //var results = await devicesQuery.GetNextAsJsonAsync();
+                //foreach (var result in results)
+                //{
+                //    Console.WriteLine(result);
+                //}
+
+                var results = await devicesQuery.GetNextAsTwinAsync();
+                foreach (var result in results)
+                {
+                    Console.WriteLine($"DeviceId={result.DeviceId}, Status={result.Status}, ConnectionState={result.ConnectionState}");
+                }
+            }
+            Console.WriteLine("Done");
         }
     }
 }
